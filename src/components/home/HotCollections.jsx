c3ea02754fe95b621a774229bd0b299499e717d9
Jadon <1tenletters0@gmail.com>
2025-05-02 09:53:11 -0700

    I really thought I was understanding this, but the slider cards are not being responsive

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -9,40 +9,40 @@ const HotCollections = ({ cards }) => {
   const [sliderRef, instanceRef] = useKeenSlider({
     loop: true,
     slides: {
+      // origin: -0.0095,
       perView: 4,
       spacing: 16,
+    },
+    breakpoints: {
+      '(max-width: 1200px)': {
+        perView: 3,
+        spacing: 1,
+      }
     },
   });
   const [isMounted, setIsMounted] = useState(false);
 
-  function Arrow(props) {
-    const disabled = props.disabled ? " arrow--disabled" : ""
-    return (
-      <svg
-        onClick={props.onClick}
-        className={`arrow ${
-          props.left ? "arrow--left" : "arrow--right"
-        } ${disabled}`}
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 24 24"
-      >
-        {props.left && (
-          <path d="M16.67 0l2.83 2.829-9.339 9.175 9.339 9.167-2.83 2.829-12.17-11.996z" />
-        )}
-        {!props.left && (
-          <path d="M5 3l3.057-3 11.943 12-11.943 12-3.057-3 9-9z" />
-        )}
-      </svg>
-    )
-  }
-  
+
+
 
   useEffect(() => {
     setIsMounted(true);
     if (instanceRef.current) {
       instanceRef.current.update();
     }
-  }, [instanceRef]);
+  }, [instanceRef, cards]);
+
+  const handleNext = () => {
+    if (instanceRef.current) {
+      instanceRef.current.next();
+    }
+  }
+
+  const handlePrev = () => {
+    if (instanceRef.current) {
+      instanceRef.current.prev();
+    }
+  }
 
   return (
     <section id="section-collections" className="no-bottom">
@@ -54,9 +54,11 @@ const HotCollections = ({ cards }) => {
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
+          <div className="navigation__wrapper">
+            <button onClick={handlePrev} className="keen__nav--left">{'<'}</button>
+            <button onClick={handleNext} className="keen__nav--right">{'>'}</button>
           {isMounted && (
             <div ref={sliderRef} className="keen-slider">
-
               {cards && cards.length > 0
                 ? cards.map((card) => (
                     <div className="keen-slider__slide" key={card.id}>
@@ -92,6 +94,7 @@ const HotCollections = ({ cards }) => {
                 : null}
             </div>
           )}
+          </div>
         </div>
       </div>
     </section>
